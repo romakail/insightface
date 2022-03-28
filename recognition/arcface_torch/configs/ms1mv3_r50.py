08dadd17f914d12b9e14f8f58d0685cbe07e7e88
Xiang <anxiangsir@outlook.com>
2022-03-28 17:47:28 +0800

    updated configs

--- recognition/arcface_torch/configs/ms1mv3_r50.py
+++ recognition/arcface_torch/configs/ms1mv3_r50.py
@@ -5,23 +5,23 @@ from easydict import EasyDict as edict
 # mount -t tmpfs -o size=140G  tmpfs /train_tmp
 
 config = edict()
-config.loss = "cosface"
-config.network = "r100"
+config.margin_list = (1.0, 0.5, 0.0)
+config.network = "r50"
 config.resume = False
 config.output = None
 config.embedding_size = 512
-config.sample_rate = 0.2
+config.sample_rate = 1.0
 config.fp16 = True
 config.momentum = 0.9
 config.weight_decay = 5e-4
-config.batch_size = 256
-config.lr = 0.3
+config.batch_size = 128
+config.lr = 0.2
 config.verbose = 2000
 config.dali = False
 
-config.rec = "/train_tmp/WebFace42M"
-config.num_classes = 2059906
-config.num_image = 42474557
-config.num_epoch = 20
-config.warmup_epoch = 1
-config.val_targets = ["lfw", "cfp_fp", "agedb_30"]
+config.rec = "/train_tmp/ms1m-retinaface-t1"
+config.num_classes = 93431
+config.num_image = 5179510
+config.num_epoch = 25
+config.warmup_epoch = 2
+config.val_targets = ['lfw', 'cfp_fp', "agedb_30"]
